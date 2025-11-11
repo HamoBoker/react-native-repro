9cbbb7920afe7044897208e4e74b832567781bba
HamoBoker <hamo.boker@gmail.com>
2025-11-11 17:14:52 +0200

    feat(App): replace NewAppScreen with blur image effects
    - Remove NewAppScreen component and related imports
    - Add NitroImage component for optimized image loading
    - Implement DanielBlurView for blur effect on image
    - Include blurred Image component for future reference
    - Update project dependencies and add new scripts for logcat management

--- App.tsx
+++ App.tsx
@@ -1,45 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React from 'react';
+import { BlurView } from '@sbaiahmed1/react-native-blur';
+import { Image, View } from 'react-native';
+import { BlurView as DanielBlurView } from '@danielsaraldi/react-native-blur-view';
+import { NitroImage } from 'react-native-nitro-image';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+    <View style={{ flex: 1, justifyContent: 'space-evenly' }}>
+      <NitroImage
+        resizeMode={'cover'}
+        image={{ url: 'https://picsum.photos/id/3/400' }}
+        style={{ height: 400 }}
       />
+      {/*<View>*/}
+      {/*  <Image height={400} source={{ uri: 'https://picsum.photos/id/3/400' }} />*/}
+      {/*  <BlurView*/}
+      {/*    blurType="dark"*/}
+      {/*    blurAmount={60}*/}
+      {/*    style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}*/}
+      {/*  />*/}
+      {/*</View>*/}
+
+      <View>
+        <Image
+          height={400}
+          source={{ uri: 'https://picsum.photos/id/3/400' }}
+        />
+        <DanielBlurView
+          targetId={'some-id'}
+          type="dark"
+          radius={20}
+          style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+        />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
